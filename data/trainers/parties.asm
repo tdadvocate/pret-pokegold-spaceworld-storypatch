495941aae97ef79e9c05864d2056d96a6a08dcf8
TD <tdhadvocate@gmail.com>
2023-07-13 01:01:49 -0400

    Fixing Rival Battle Bug
    Looks like in a previous commit I had been attempting to fix the grammatical error in the Rival battle and forgot to revert the changes. This caused the party to revert to the bugged format. I have now fixed this issue and hopefully you should be able to compile the latest version again properly.

--- data/trainers/parties.asm
+++ data/trainers/parties.asm
@@ -165,13 +165,13 @@ GamaGroup::
 	db 37, MON_GROWLITHE, MON_VULPIX, 0
 
 RivalGroup::
-	db "", TRAINERTYPE_NORMAL ;	used to be db  5, DEX_KURUSU,0
+	db "@", TRAINERTYPE_NORMAL ;	used to be db  5, DEX_KURUSU,0
 	db  5, DEX_KURUSU
 	db -1 ; end
-	db "", TRAINERTYPE_NORMAL ;	used to be db  5, DEX_HAPPA,0
+	db "@", TRAINERTYPE_NORMAL ;	used to be db  5, DEX_HAPPA,0
 	db  5, DEX_HAPPA
 	db -1 ; end
-	db "", TRAINERTYPE_NORMAL ;	used to be db  5, DEX_HONOGUMA,0
+	db "@", TRAINERTYPE_NORMAL ;	used to be db  5, DEX_HONOGUMA,0
 	db  5, DEX_HONOGUMA
 	db -1 ; end
 ; Leftover Hiker data from Red/Blue
