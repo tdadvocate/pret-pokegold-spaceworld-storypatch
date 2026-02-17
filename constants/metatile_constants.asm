a8759501b4c0abf93d5ca8dfc8c678877f347a39
TD <tdhadvocate@gmail.com>
2026-02-16 23:31:51 -0500

    Cut Tree & Water Spawn Fixes
    Changes:
    
    -Finally fixed the cut tree in Old City
    
    -Created custom tiles for Silent to have a cut tree to the East temporarily to make sure people don't soft lock their game without the starter
    
    -Fixing water spawns for all water Routes hopefully (I think only tilesets 0, 1, 3, 5, & 6 have spawns but editing others just in case)

--- constants/metatile_constants.asm
+++ constants/metatile_constants.asm
@@ -17,4 +17,8 @@ DEF METATILE_CUT_NE_TREE_NW EQU $32 ; T C / L L
 DEF METATILE_CUT_NE_TREE_SE EQU $33 ; L C / L T
 DEF METATILE_SMALL_TREE_NW  EQU $34 ; T L / L L
 DEF METATILE_SMALL_TREE_SE  EQU $35 ; T L / L L
+DEF METATILE_NOT_CUT_SILENT EQU $65 ; D D / D C - Manually created for temp Silent cut tree (not cut)
+DEF METATILE_YES_CUT_SILENT EQU $1B ; T C / D D - Manually created for temp Silent cut tree (tree gone)
+DEF METATILE_NOT_CUT_OLD    EQU $6C ; T C / D D - Manually created for cut tree in Old (not cut)
+DEF METATILE_YES_CUT_OLD    EQU $75 ; T C / D D - Manually created for cut tree in Old (tree gone)
 DEF METATILE_GRASS          EQU $3b ; G G / G G
